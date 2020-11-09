1972f7934659da38710f05a35ba7f4c1f7fff021
FrancescoS <francesco.salerno2294@gmail.com>
2020-11-09 16:59:20 +0100

    Work2 Database

--- database/migrations/2020_11_08_154015_create_flats_table.php
+++ database/migrations/2020_11_08_154015_create_flats_table.php
@@ -21,7 +21,7 @@ class CreateFlatsTable extends Migration
             $table->integer('mq');
             $table->string('image');
             $table->text('description', 500);
-            $table->dataTime('end_rate');
+            $table->dateTime('end_rate');
             $table->timestamps();
         });
     }
--- database/migrations/2020_11_08_154558_create_flat_service_table.php
+++ database/migrations/2020_11_08_154558_create_flat_service_table.php
@@ -14,7 +14,14 @@ class CreateFlatServiceTable extends Migration
     public function up()
     {
         Schema::create('flat_service', function (Blueprint $table) {
-            $table->id();
+            // $table->id();
+
+            // PROVA
+            $table->unsignedBigInteger('flat_id');
+            $table->foreign('flat_id')->references('id')->on('flats')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->unsignedBigInteger('service_id');
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
