6a82749be8f00aa717f3e375ad887f5c94bbfd63
Valeria Curseri <curserivaleria@gmail.com>
2020-11-12 12:14:03 +0100

    Aggiornamento table messages (faker email da users)

--- database/seeds/MessagesTableSeeder.php
+++ database/seeds/MessagesTableSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Message;
 use App\Flat;
+use App\User;
 
 class MessagesTableSeeder extends Seeder
 {
@@ -15,11 +16,12 @@ class MessagesTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         $flats = Flat::all();
-        for($i = 0; $i < 10; $i++) {
+        $users = User::all();
+        for($i = 0; $i < 100; $i++) {
             $newMessage = new Message();
             $newMessage->name = $faker->firstname;
             $newMessage->lastname = $faker->lastname;
-            $newMessage->email = $faker->email;
+            $newMessage->email = $users->random()->email;
             $newMessage->request = $faker->realText();
             $newMessage->viewed = $faker->numberBetween(0, 1);
             $newMessage->flat_id = $flats->random()->id;
