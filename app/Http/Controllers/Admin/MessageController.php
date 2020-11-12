7a11da5725679594c6da81a083790e3c9b311a4f
Valeria Curseri <curserivaleria@gmail.com>
2020-11-12 16:00:48 +0100

    fz e view show messages

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -38,6 +38,10 @@ class MessageController extends Controller
 
         }
     }
+
+    public function show(Message $message){
+        return view('admin.messages.show', compact('message'));
+    }
     
     public function create(){
         // $tags = Tag::all();
@@ -69,15 +73,6 @@ class MessageController extends Controller
         // };
     }
     
-    public function show(Post $post){
-        // $users = User::all();
-        // $id = $post['user_id'];
-        // $user = $users->find($id);
-        // $nomeUtente = $user['name'];
-        // $tags = $post['tags'];
-        // return view('admin.posts.show', compact('post', 'nomeUtente','tags'));
-    }
-    
     public function destroy(Post $post){
         // $post->delete();
         // return redirect()->route('admin.posts.index')->with('status','Articolo cancellato correttamente');
@@ -88,40 +83,18 @@ class MessageController extends Controller
         // return view('admin.posts.create', compact('post','tags'));
     }
     
-    public function update(Request $request, Post $post){
-        // $data = $request->all();
-        
-        // $request->validate([
-        //     // 'titolo' => 'required|unique:posts',
-        //     // 'articolo' => 'required|unique:posts',
-        //     'titolo' => [
-        //         'required',
-        //         Rule::unique('posts')->ignore($post), // regola che permette di non controllare rispetto all'unicità anche il titolo che sto modidficando. NECESSARIO INSERIRE use Illuminate\Validation\Rule;
-        //     ],
-        //     'articolo' => [
-        //         'required',
-        //         Rule::unique('posts')->ignore($post),
-        //     ],
-        //     'img'=> 'image'
-        // ]);
-        
-        // $data['slug'] = Str::slug($data['titolo'], '-');
 
-        // if(empty($data['tags'])){                   // SE l'array dei tags è vuoto
-        //     $post->tags()->detach();                    // elimino tutti i tags salvati
-        // } else {                                    // ALTRIMENTI se l'array dei tags non è vuoto e ci sono modifiche
-        //     $post->tags()->sync($data['tags']);         // con sync aggiorno i tags salvati
-        // }
-        
-        // if(!empty($data['img'])){                   // SE da form è stata inserita una nuova immagine
-        //     if(!empty($post['img'])){               // E SE era già stata caricata un'immagine per questo articolo
-        //         Storage::disk('public')->delete('images',$post['img']); // all'interno della cartella public > storage > images cancello l'immagine memorizzata in precedenza
-        //     }
-        //     $data['img'] = Storage::disk('public')->put('images',$data['img']); // all'interno della stessa cartella memorizzo il nuovo file
-        // }
+    // TENTATIVO PER SOVRASCRIVERE VALORE VIEWED MA NON ENTRA IN UPDATE
 
-        // $post->update($data);
+    public function update(Request $request, Message $message){
+
+        $data = $request->all();
+        dd($data);
         
-        // return redirect()->route('admin.posts.index')->with('status','Articolo modificato correttamente');
+        $data['viewed'] = !$message['viewed'];
+
+        $message->update($data);
+        
+        return redirect()->route('admin.messages.index')->with('status','Messaggio impostato come letto');
     }
 }
--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -3,6 +3,13 @@
 @section('content')
 <div class="container">
   <h1>Tutti i messaggi ricevuti</h1>
+
+  @if(session('status'))
+      <div class="alert alert-success">
+          {{ session('status') }}
+      </div>
+  @endif
+
   <table class="table">
     <thead>
       <tr>
@@ -18,11 +25,13 @@
     <tbody>
       @foreach($messages as $message)
       <tr>
-        <th scope="row">{{ $message['name'] }}</th>
+        <td>{{ $message['name'] }}</td>
         <td>{{ $message['lastname'] }}</td>
         <td>{{ $message['email'] }}</td>
         <td>{{ $message['flat_id'] }}</td>
-        <td>Leggi</td>
+        <td class="align-middle">
+          <a href="{{ route('admin.messages.show', $message['id']) }}" class="btn btn-dark">Leggi messaggio</a>
+        </td>
         <td>Segna come letto</td>
         <td>Elimina</td>
       </tr>
