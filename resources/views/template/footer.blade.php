67801ed89e1fb76131b39796d2ad3da1c3277288
LucaRiccio <lucariccio888@gmail.com>
2020-11-13 16:51:05 +0100

    Bozza footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,1 +1,73 @@
-questo è il mio bel footer
+<div class="container-fluid" id="footer">
+  <div class="row">
+    <div class="col">
+      <ul>
+        <a href="#"><li><strong>INFORMAZIONI</strong></li></a>
+        <a href="#"><li>Newsroom</li></a>
+        <a href="#"><li>Airbnb Plus</li></a>
+        <a href="#"><li>Airbnb Luxe</li></a>
+        <a href="#"><li>HotelTonight</li></a>
+        <a href="#"><li>Airbnb for Work</li></a>
+        <a href="#"><li>Olimpiadi</li></a>
+        <a href="#"><li>Opportunità di lavoro</li></a>
+      </ul>
+    </div>
+
+    <div class="col">
+      <ul>
+        <a href="#"><li><strong>COMMUNITY</strong></li></a>
+        <a href="#"><li>Diversità e appartenenza</li></a>
+        <a href="#"><li>Accessibilità</li></a>
+        <a href="#"><li>Airbnb Associates</li></a>
+        <a href="#"><li>Alloggi per l'emergenza</li></a>
+        <a href="#"><li>Invita degli amici</li></a>
+        <a href="#"><li>Olimpiadi</li></a>
+        <a href="#"><li>Opportunità di lavoro</li></a>
+      </ul>
+    </div>
+
+    <div class="col">
+      <ul>
+        <a href="#"><li><strong>OSPITA</strong></li></a>
+        <a href="#"><li>Diventa un host</li></a>
+        <a href="#"><li>Offri un'Esperienza online</li></a>
+        <a href="#"><li>Offri un'Esperienza</li></a>
+        <a href="#"><li>Ospitare responsabilmente</li></a>
+        <a href="#"><li>Open Homes</li></a>
+        <a href="#"><li>Centro Risorse</li></a>
+        <a href="#"><li>Community Center</li></a>
+      </ul>
+    </div>
+
+    <div class="col">
+      <ul>
+        <a href="#"><li><strong>ASSISTENZA</strong></li></a>
+        <a href="#"><li>La nostra risposta all'emergenza COVID-19</li></a>
+        <a href="#"><li>Centro Assistenza</li></a>
+        <a href="#"><li>Opzioni di cancellazione</li></a>
+        <a href="#"><li>Servizio di supporto al vicinato</li></a>
+        <a href="#"><li>Affidabilità e sicurezza</li></a>
+      </ul>
+    </div>
+
+  </div>
+
+  <div class="row" id="footer-bottom">
+
+    <div class="col" id="footer-bottom-left">
+      <p>© 2020 Boolbnb, Inc. All rights reserved</p>
+      <p>Privacy</p>
+      <p>Termini</p>
+      <p>Dettagli dell'azienda</p>
+    </div>
+
+    <div class="col">
+      <span><i class="fas fa-globe"></i>Italiano (IT)</span>
+      <span><i class="fas fa-euro-sign"></i>EUR</span>
+      <span><i class="fab fa-facebook-f"></i></span>
+      <span><i class="fab fa-twitter"></i></span>
+      <span><i class="fab fa-instagram-square"></i></span>
+    </div>
+
+  </div>
+</div>
