b82da86441105fb8c734aa1b69ec436d3fd1dec3
Valeria Curseri <curserivaleria@gmail.com>
2020-11-13 14:47:02 +0100

    commenti + filtro sponsorizzate in search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,17 +25,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $momentoAttuale = date("Y-m-d H:i:s");  // sistemare ora legale
-        $sponsAttive = Payment::all()->where('end_rate', '>', $momentoAttuale);
-        $flatsIdSpons = [];
+        $momentoAttuale = date("Y-m-d H:i:s"); // ATTENZIONE sistemare ora legale  memorizzo in una var data e ora attuale, nello stesso formato del created_at 
+        $sponsAttive = Payment::all()->where('end_rate', '>', $momentoAttuale); // memorizzo in una var tutti i pagamenti con end_rate successivo al momento attuale (vuol dire che sono sponsorizzati)
+        $flatsIdSpons = [];                                                     // imposto un array vuoto
         foreach($sponsAttive as $sponsAttiva){
-            array_push($flatsIdSpons, $sponsAttiva->flat_id);
+            array_push($flatsIdSpons, $sponsAttiva->flat_id);                   // memorizzo nell'array vuoto tutti gli id degli appartamenti sponsorizzati
         }
-        // dd($flatsIdSpons);
-        
-        $flatsSpons = Flat::all()->whereIn('id', $flatsIdSpons);
-        // dd($flatsSpons);
-
+        $flatsSpons = Flat::all()->whereIn('id', $flatsIdSpons);                // memorizzo in una var tutti gli appartamenti con id contenuto nell'array degli id degli appartamenti sponsorizzati
         return view('home', compact('flatsSpons'));
     }
 }
