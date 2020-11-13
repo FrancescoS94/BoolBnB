ee509c6f11166d0646e55457d80ebe1347b538c2
Valeria Curseri <curserivaleria@gmail.com>
2020-11-13 16:39:16 +0100

    create di un nuovo indirizzo

--- app/Http/Controllers/Admin/AddressController.php
+++ app/Http/Controllers/Admin/AddressController.php
@@ -4,8 +4,31 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Address;
 
 class AddressController extends Controller
 {
-    //
+    function store(Request $request, Address $address){
+
+        $data = $request->all();
+
+        // $request->validate([ #validazione e controllo dei dati passati
+        //     'title' => 'required|string|max:200|min:20',
+        //     'room' => 'required|numeric|max:20|min:1',
+        //     'bed' => 'required|numeric|max:20|min:1',
+        //     'wc' => 'required|numeric|max:10|min:1',
+        //     'mq' => 'required|numeric|max:1000|min:15',
+        //     'description' => 'required|max:1200|min:15',
+        //     'image' => 'image|required'
+        // ]);
+
+        $address->fill($data); #riempio i vari campi dopo la validazione
+        $address->save();
+
+        if($address->save()){
+            return redirect()->route('admin.flats.create', compact('address'))->with('status', 'Hai aggiunto correttamente l\'indirizzo del nuovo appartamento');
+        }else{
+            abort(404);
+        }
+    }
 }
--- resources/views/admin/flats/flats-create.blade.php
+++ resources/views/admin/flats/flats-create.blade.php
@@ -6,6 +6,12 @@
             <div class="col-md-8">
 
                 <h1>Aggiungi un'appartamento</h1>
+                {{-- status --}}
+                @if(session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
                 {{-- controllo errori --}}
                 @if ($errors->any())
                 <div class="alert alert-danger">
@@ -16,13 +22,47 @@
                     </ul>
                 </div>
                 @endif
+                <h2>Dove si trova l'appartamento che vuoi inserire?</h2>
+                {{-- form creazione indirizzo, punta al controller Admin/AddressController  --}}
+                <form action="{{ route('admin.addresses.store') }}" method="post">
+                    @csrf
+                    @method('POST')
+
+                    <div class="form-group">
+                        <label for="country">Nazione</label>
+                        <input type="text" class="form-control" name="country" id="country">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="city">Città</label>
+                        <input type="text" class="form-control" name="city" id="city">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="address">Indirizzo</label>
+                        <input type="text" class="form-control" name="address" id="address">
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="cap">CAP</label>
+                        <input type="text" class="form-control" name="cap" id="cap">
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="district">Provincia</label>
+                        <input type="text" class="form-control" name="district" id="district">
+                    </div>
+           
+                    <button type="submit" class="btn btn-primary">Registra l'indirizzo dell'appartamento</button>
+                </form>
                 
+                <h2>Descrivi il tuo appartamento a i nostri utenti</h2>
+    
                 {{-- form creazione punta al Admin/controllerFlat  --}}
-                <form action="{{ route('admin.flats.store')}}" method="post" enctype="multipart/form-data">
+                <form action="{{ route('admin.flats.store')" method="post" enctype="multipart/form-data">
                     @csrf
                     @method('POST')
     
-
                     <div class="form-group">
                         <label for="title">Titolo</label>
                         <input type="text" class="form-control" name="title">
@@ -58,7 +98,7 @@
                         <input type="file" class="form-control-file" name="image">
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Invia il modulo</button>
+                    <button type="submit" class="btn btn-primary">Registra l'appartamento</button>
                 </form>
             </div>
         </div>
