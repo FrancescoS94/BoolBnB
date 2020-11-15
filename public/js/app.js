c3937e13ea1fbba5b31210eb2397fd2f867cf9c5
LucaRiccio <lucariccio888@gmail.com>
2020-11-15 19:20:25 +0100

    Bozza Search (prova layout mappa)

--- public/js/app.js
+++ public/js/app.js
@@ -37341,8 +37341,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Valeria\Dropbox\Coding\Boolean Careers\Corso\mamp_public\lavori-php\BoolBnB\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Valeria\Dropbox\Coding\Boolean Careers\Corso\mamp_public\lavori-php\BoolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\mamp_public\BoolBnB\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\mamp_public\BoolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,37 +1,39 @@
 {{-- PAGINA DI RICERCA --}}
 @extends('layouts.app')
 @section('content')
-
+  
 <h2>SEARCH</h2>
 
 {{-- RAGA per visualizzare questa parte in homepage dovete
     1. andare su mysql nella tabella payments
     2. impostare a qualche pagamento un valore end_rate la data di domani --}}
 
-
 <div class="container-fluid search">
 
-  @foreach ($flatsSpons as $flatSpons)
-    <div class="row flat">
+    <div class="row">
 
-      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3">
-        <img id="img-search" src="{{ $flatSpons->image }}" class="card-img-top" alt="{{ $flatSpons->title}}"> {{-- immagine della casa. non si vede perchè lorempicsum fa schifo, dovremmo acambiare delle impostazioni --}}
-      </div>
+      <div class="col">
+        @foreach ($flatsSpons as $flatSpons)
+        <div class="row flat">
+          <div class="col-xl-5 col-lg-5 col-md-5 col-sm-5">
+            <img id="img-search" src="{{ $flatSpons->image }}" class="card-img-top" alt="{{ $flatSpons->title}}">
+          </div>
 
-      <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-        <div class="">
-          <h5 class="card-title">{{ $flatSpons->title}}</h5>{{-- titolo dell'appartamento --}}
-          <p class="card-text">{{ $flatSpons->description}}</p>{{-- descrizione dell'appartamento --}}
-          <a href="{{ route('flat', $flatSpons->id) }}" class="btn btn-primary">Vai alla show dell'appartamento</a> {{-- bottone per andare nella show dell'appartamento --}}
+          <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+            <div class="">
+              <h5 class="card-title">{{ $flatSpons->title}}</h5>
+              <p class="card-text">{{ $flatSpons->description}}</p>
+              <a href="{{ route('flats.show', $flatSpons->id) }}" class="btn btn-primary">Vai alla show dell'appartamento</a>
+            </div>
+          </div>
         </div>
+        @endforeach
       </div>
-
-      <div class="col-xl-5 col-lg-5 col-md-5 col-sm-5 map">
-
+      <div class="col-4 mappa">
+         mappa
       </div>
 
     </div>
-  @endforeach
 
 </div>
 
