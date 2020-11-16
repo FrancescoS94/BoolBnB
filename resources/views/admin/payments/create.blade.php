de1cd78bf80d58e2c10f73c4fa3d651e7e674c02
Valeria Curseri <curserivaleria@gmail.com>
2020-11-16 11:14:52 +0100

    id in form address, cancellato dd inutile in home

--- resources/views/admin/payments/create.blade.php
+++ resources/views/admin/payments/create.blade.php
@@ -1,4 +1,33 @@
 @extends('layouts.app')
 @section('content')
+    <div class="container">
+        <h1>Sponsorizza l'appartamento {{$flat->id}}</h1>
+
+        {{-- controllo errori --}}
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        
+        <p>Puoi sponsorizzare l'appartamento {{ $flat->title}} per 1, 3 o 6 giorni.</p>
+        <p>Il tuo appartamento verrà mostrato in home page e nella pagina di ricerca, in evidenza rispetto agli altri appartamenti, per l'intera durata della sponsorizzazione.</p>
+        
+        {{-- form per la sponsorizzazione dell'appartamento, punta al controller Admin/PaymentController  --}}
+        <form action="{{ route('admin.payments.store') }}" method="post">
+            @csrf
+            @method('POST')
+
+            <div class="form-group">
+                <label for="end_rate">Scegli la tua sponsorizzazione</label>
+                <input id="end_rate" type="text" class="form-control" name="end_rate">
+            </div>
     
+            <button type="submit" class="btn btn-primary">Sponsorizza l'appartamento</button>
+        </form>
+    </div>
 @endsection
