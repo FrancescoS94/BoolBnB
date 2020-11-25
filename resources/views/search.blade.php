1ff2b59537d153e3ed2762749fb757dcc2aeada8
LucaRiccio <lucariccio888@gmail.com>
2020-11-25 15:48:25 +0100

    Nuova Flats

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,11 +4,11 @@
 
 <div class="container-fluid layout">
 
-  <div class="row filtri">
+  {{-- <div class="row filtri">
     <div class="col-12 filters">
       <p>FILTRI</p>
     </div>
-  </div>
+  </div> --}}
 
   <section class="container-fluid sponsor">
     <h2>Scorri i nostri migliori appartamenti</h2>
@@ -30,42 +30,41 @@
     </div>
   </section>
 
-
-  <div class="container-fluid ricerca">
-    @foreach ($flatsSpons as $flatSpons)
+  <div class="container ricerca">
+    @foreach ($flatsInRadius as $flat)
     <div class="row search">
-      <div class="col-5">
-        <a href="{{ route('flats.show', $flatSpons->id) }}"><img id="img-search" src="{{ asset('storage/'.$flatSpons->image ) }}" class="img-fluid" alt="{{ $flatSpons->title}}"></a>
+      <div class="foto col-9 col-sm-6 col-md-6 col-lg-6 col-xl-5 ">
+        <a href="{{ route('flats.show', $flat->id) }}"><img id="img-search" src="{{ asset('storage/'.$flat->image ) }}" class="img-fluid" alt="{{ $flat->title}}"></a>
       </div>
 
-      <div class="col-6">
+      <div class="my-auto col-9 col-sm-6 col-md-6 col-lg-6 col-xl-6 ">
         <div class="flat-text">
-          <a href="{{ route('flats.show', $flatSpons->id) }}">
-            <h5 class="card-title">{{ $flatSpons->title}}</h5>
-            <p>{{ $flatSpons->address->address }}</p>
+          <a href="{{ route('flats.show', $flat->id) }}">
+            <h5 class="card-title">{{ $flat->title}}</h5>
+            <p>{{ $flat->address->address }}</p>
             <ul>
               <li>
                 <img src="https://www.flaticon.com/svg/static/icons/svg/2286/2286105.svg" alt="">
-                Letti: {{$flatSpons->bed}}
+                Letti: {{$flat->bed}}
               </li>
               <li>
                 <img src="https://www.flaticon.com/svg/static/icons/svg/578/578059.svg" alt="">
-                Stanze: {{$flatSpons->room}}
+                Stanze: {{$flat->room}}
               </li>
             </ul>
               <ul>
                 <li>
                   <img src="https://www.flaticon.com/svg/static/icons/svg/3030/3030330.svg" alt="">
-                  WC: {{$flatSpons->wc}}
+                  WC: {{$flat->wc}}
                 </li>
                 <li>
                   <img src="https://www.flaticon.com/svg/static/icons/svg/515/515159.svg" alt="">
-                  Mq: {{$flatSpons->mq}}
+                  Mq: {{$flat->mq}}
                 </li>
               </ul>
           </a>
           <div class="flat-service">
-            @foreach($flatSpons->services as $service)
+            @foreach($flat->services as $service)
             <span> {{ $service->service }} </span>
             @endforeach
           </div>
@@ -73,11 +72,9 @@
       </div>
 
     </div> {{-- chiusura row search--}}
-  @endforeach
+    @endforeach
 
-</div> {{-- chiusura container-fluid ricerca--}}
-
-
+  </div> {{-- chiusura container-fluid ricerca--}}
 
 </div> {{-- chiusura layout --}}
 
