5706483345f1d6134941a07b11d1a5f3f1fd9b95
Valeria Curseri <curserivaleria@gmail.com>
2020-11-25 15:02:31 +0100

    braintree funzionante

--- resources/views/admin/payments/create.blade.php
+++ resources/views/admin/payments/create.blade.php
@@ -1,153 +1,245 @@
 @extends('layouts.admin')
+
+@section('script-in-head')
+    {{-- STYLE BRAINTREE PER CAMPI DATI CARTA DI CREDITO --}}
+    <style>
+        #card-number, #cvv, #expiration-date{
+            background:white;
+            height: 38px;
+            border: 1px solid #ced4da;
+            padding: .375rem .75rem;
+            border-radius: .25rem;
+        }
+    </style>
+@endsection
+
 @section('content')
 
-    <style>
-                <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-<!-- Styles -->
-<style>
-    html, body {
-        background-color: #fff;
-        color: #636b6f;
-        font-family: 'Nunito', sans-serif;
-        font-weight: 200;
-        height: 100vh;
-        margin: 0;
-    }
-    .full-height {
-        height: 100vh;
-    }
-    .flex-center {
-        align-items: center;
-        display: flex;
-        justify-content: center;
-    }
-    .position-ref {
-        position: relative;
-    }
-    .top-right {
-        position: absolute;
-        right: 10px;
-        top: 18px;
-    }
-    .content {
-        text-align: center;
-    }
-    .title {
-        font-size: 84px;
-    }
-    .links > a {
-        color: #636b6f;
-        padding: 0 25px;
-        font-size: 13px;
-        font-weight: 600;
-        letter-spacing: .1rem;
-        text-decoration: none;
-        text-transform: uppercase;
-    }
-    .m-b-md {
-        margin-bottom: 30px;
-    }
-</style>
-
-
+    {{-- SCRIPT BRAINTREE --}}
+    <script src="https://js.braintreegateway.com/web/dropin/1.25.0/js/dropin.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/3.38.1/js/client.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/3.38.1/js/hosted-fields.min.js"></script>
+    
     <div class="container">
         <h1>Sponsorizza un appartamento</h1>
 
-        {{-- controllo errori --}}
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
+        {{-- inserire costi sponsorizzazioni --}}
 
         <p>Puoi sponsorizzare un appartamento per 1, 3 o 6 giorni.</p>
         <p>Il tuo appartamento verrà mostrato in home page e nella pagina di ricerca, in evidenza rispetto agli altri appartamenti, per l'intera durata della sponsorizzazione.</p>
 
-        {{-- form per la sponsorizzazione dell'appartamento, punta al controller Admin/PaymentController  --}}
-        <form action="{{ route('admin.payments.store') }}" method="post">
-            @csrf
-            @method('POST')
+        {{-- HOSTED FORM BRAINTREE --}}
 
-            <div class="form-group">
-                <label for="flat_id">Scegli l'appartamento che vuoi sponsorizzare</label>
-                <select class="form-control" id="flat_id" name="flat_id">
-                    <option>Seleziona un appartamento</option>
-                    @foreach($flats as $flat)
-                    <option value="{{ $flat->id }}">{{ $flat->title }}</option>
-                    {{-- <option>{{ $flat->id }}</option> --}}
-                    @endforeach
-                </select>
-            </div>
+        <div class="col-md-6 offset-md-3">
+            <div class="spacer"></div>
 
-            <div class="form-group">
-                <label for="rate_id">Scegli la tua sponsorizzazione</label>
-                <select class="form-control" id="rate_id" name="rate_id">
-                    <option>Seleziona una tipologia di sponsorizzazione</option>
-                    <option value="1">24 ore - 1 giorno</option>
-                    <option value="2">72 ore - 3 giorni</option>
-                    <option value="3">144 ore - 6 giorni</option>
-                </select>
-            </div>
+            @if (session()->has('success_message'))
+                <div class="alert alert-success">
+                    {{ session()->get('success_message') }}
+                </div>
+            @endif
 
-            <button type="submit" class="btn btn-primary">Sponsorizza l'appartamento</button>
-        </form>
-
-        {{-- <div class="content">
-            form pagamento
-            <form method="post" id="payment-form" action="#">
+            @if(count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="{{ route('admin.payments.store') }}" method="POST" id="payment-form">
                 @csrf
-                <section>
-                    <label for="amount">
-                        <span class="input-label">Amount</span>
-                        <div class="input-wrapper amount-wrapper">
-                            <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
-                        </div>
-                    </label>
 
-                    <div class="bt-drop-in-wrapper">
-                        <div id="bt-dropin"></div>
+                <div class="form-group">
+                    <label for="email">Email Address</label>
+                    <input type="email" class="form-control" id="email">
+                </div>
+
+                <div class="form-group">
+                    <label for="name_on_card">Name on Card</label>
+                    <input type="text" class="form-control" id="name_on_card" name="name_on_card">
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="address">Address</label>
+                            <input type="text" class="form-control" id="address" name="address">
+                        </div>
                     </div>
-                </section>
+
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="city">City</label>
+                            <input type="text" class="form-control" id="city" name="city">
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="province">Province</label>
+                            <input type="text" class="form-control" id="province" name="province">
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="postalcode">Postal Code</label>
+                            <input type="text" class="form-control" id="postalcode" name="postalcode">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="country">Country</label>
+                            <input type="text" class="form-control" id="country" name="country">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="phone">Phone</label>
+                            <input type="text" class="form-control" id="phone" name="phone">
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="form-group">
+                    <label for="flat_id">Scegli l'appartamento che vuoi sponsorizzare</label>
+                    <select class="form-control" id="flat_id" name="flat_id">
+                        <option>Seleziona un appartamento</option>
+                        @foreach($flats as $flat)
+                        <option value="{{ $flat->id }}">{{ $flat->title }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="rate_id">Scegli la tua sponsorizzazione</label>
+                    <select class="form-control" id="rate_id" name="rate_id">
+                        <option>Seleziona una tipologia di sponsorizzazione</option>
+                        <option value="1">24 ore - 1 giorno</option>
+                        <option value="2">72 ore - 3 giorni</option>
+                        <option value="3">144 ore - 6 giorni</option>
+                    </select>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="cc_number">Credit Card Number</label>
+
+                        <div class="form-group" id="card-number">
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="expiry">Expiry</label>
+
+                        <div class="form-group" id="expiration-date">
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="cvv">CVV</label>
+
+                        <div class="form-group" id="cvv">
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="spacer"></div>
+
+                <div id="paypal-button"></div>
+
+                <div class="spacer"></div>
 
                 <input id="nonce" name="payment_method_nonce" type="hidden" />
-                <button class="button" type="submit"><span>Test Transaction</span></button>
+                <button type="submit" class="btn btn-success">Submit Payment</button>
             </form>
-        </div> --}}
+        </div>
     </div>
+    {{-- SCRIPT HOSTED FORM --}}
 
-
-    {{-- <script src="https://js.braintreegateway.com/web/dropin/1.13.0/js/dropin.min.js"></script>
     <script>
-        var form = document.querySelector('#payment-form');
-        /* var client_token = "{{ $token }}"; */ /* {!! json_encode($token->toArray()) !!}; */  /* "{{ $token }}" */
-        braintree.dropin.create({
-          authorization: client_token,
-          selector: '#bt-dropin',
-          paypal: {
-            flow: 'vault'
+      var form = document.querySelector('#payment-form');
+      var submit = document.querySelector('input[type="submit"]');
+
+      braintree.client.create({
+        authorization: '{{ $token }}'
+      }, function (clientErr, clientInstance) {
+        if (clientErr) {
+          console.error(clientErr);
+          return;
+        }
+
+        // This example shows Hosted Fields, but you can also use this
+        // client instance to create additional components here, such as
+        // PayPal or Data Collector.
+
+        braintree.hostedFields.create({
+          client: clientInstance,
+          styles: {
+            'input': {
+              'font-size': '14px'
+            },
+            'input.invalid': {
+              'color': 'red'
+            },
+            'input.valid': {
+              'color': 'green'
+            }
+          },
+          fields: {
+            number: {
+              selector: '#card-number',
+              placeholder: '4111 1111 1111 1111'
+            },
+            cvv: {
+              selector: '#cvv',
+              placeholder: '123'
+            },
+            expirationDate: {
+              selector: '#expiration-date',
+              placeholder: '10/2022'
+            }
           }
-        }, function (createErr, instance) {
-          if (createErr) {
-            console.log('Create Error', createErr);
+        }, function (hostedFieldsErr, hostedFieldsInstance) {
+          if (hostedFieldsErr) {
+            console.error(hostedFieldsErr);
             return;
           }
+
           form.addEventListener('submit', function (event) {
             event.preventDefault();
-            instance.requestPaymentMethod(function (err, payload) {
-              if (err) {
-                console.log('Request Payment Method Error', err);
+
+            hostedFieldsInstance.tokenize(function (tokenizeErr, payload) {
+              if (tokenizeErr) {
+                console.error(tokenizeErr);
                 return;
               }
-              // Add the nonce to the form and submit
+
+              // If this was a real integration, this is where you would
+              // send the nonce to your server.
+            //   console.log('Got a nonce: ' + payload.nonce);
+                // Add the nonce to the form and submit
               document.querySelector('#nonce').value = payload.nonce;
               form.submit();
             });
-          });
+          }, false);
         });
-    </script> --}}
+      });
+    </script>
+
+    
 @endsection
