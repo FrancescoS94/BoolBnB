40b6a23fd54645484d0343b3a0fc2e6cd1192ec5
LucaRiccio <lucariccio888@gmail.com>
2020-11-26 11:13:48 +0100

    Bozza Show Messaggio

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -2,13 +2,13 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
+<div class="container messaggio-ricevuto ">
 
-  <div class="navigazione">
+  {{-- <div class="navigazione">
     <a href="{{ route('admin.messages.index') }}">Torna a tutti i messaggi</a>
-  </div>
+  </div> --}}
 
-  <h1>Messaggio ricevuto</h1>
+  {{-- <h1>Messaggio ricevuto</h1> --}}
   <div class="dettagli">
     <ul>
       <li> <strong>Data e ora di ricezione</strong> {{ Carbon\Carbon::parse($message['created_at'])->settings(['toStringFormat' => 'j F Y \a\l\l\e h:i:s', ]) }}</li>
@@ -17,8 +17,12 @@
       <li> <strong>Email</strong> {{ $message['email'] }}</li>
       <li> <strong>Appartamento</strong> {{ $message['flat_id'] }}</li>
     </ul>
+  </div>
+
+  <div class="testo-messaggio">
     <p>{{ $message['request'] }}</p>
   </div>
+
   <div class="lettura">
     @if($message['viewed'] == 0)
       <form action="{{ route('admin.messages.update', $message['id']) }}" method="post">
@@ -26,18 +30,21 @@
           @method('PATCH')
           <input class="btn btn-dark" type="submit" value="Segna come letto">
       </form>
-      @else
+
+    @else
       <form action="{{ route('admin.messages.update', $message['id']) }}" method="post">
           @csrf
           @method('PATCH')
           <input class="btn btn-dark" type="submit" value="Segna come da leggere">
       </form>
+
     @endif
-    <form action="{{ route('admin.messages.destroy', $message['id']) }}" method="post">
-        @csrf
-        @method('DELETE')
-        <input class="btn btn-dark" type="submit" value="Cancella messaggio">
-    </form>
+      <form action="{{ route('admin.messages.destroy', $message['id']) }}" method="post">
+          @csrf
+          @method('DELETE')
+          <input class="btn btn-dark" type="submit" value="Cancella messaggio">
+      </form>
+
   </div>
 </div>
 @endsection
