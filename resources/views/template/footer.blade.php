f5f368ffc8ad7485550f4f04503af755091ee28d
Valeria Curseri <curserivaleria@gmail.com>
2020-11-26 17:23:53 +0100

    footer sistemato

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,73 +1,77 @@
-<div class="container" id="footer">
-  <div class="row">
-    <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3">
-      <ul>
-        <a href="#"><li><strong>INFORMAZIONI</strong></li></a>
-        <a href="#"><li>Newsroom</li></a>
-        <a href="#"><li>Airbnb Plus</li></a>
-        <a href="#"><li>Airbnb Luxe</li></a>
-        <a href="#"><li>HotelTonight</li></a>
-        <a href="#"><li>Airbnb for Work</li></a>
-        <a href="#"><li>Olimpiadi</li></a>
-        <a href="#"><li>Opportunità di lavoro</li></a>
-      </ul>
+<div id="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+        <ul>
+          <a href="#"><li><strong>INFORMAZIONI</strong></li></a>
+          <a href="#"><li>Newsroom</li></a>
+          <a href="#"><li>Airbnb Plus</li></a>
+          <a href="#"><li>Airbnb Luxe</li></a>
+          <a href="#"><li>HotelTonight</li></a>
+          <a href="#"><li>Airbnb for Work</li></a>
+          <a href="#"><li>Olimpiadi</li></a>
+          <a href="#"><li>Opportunità di lavoro</li></a>
+        </ul>
+      </div>
+  
+      <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+        <ul>
+          <a href="#"><li><strong>COMMUNITY</strong></li></a>
+          <a href="#"><li>Diversità e appartenenza</li></a>
+          <a href="#"><li>Accessibilità</li></a>
+          <a href="#"><li>Airbnb Associates</li></a>
+          <a href="#"><li>Alloggi per l'emergenza</li></a>
+          <a href="#"><li>Invita degli amici</li></a>
+          <a href="#"><li>Olimpiadi</li></a>
+          <a href="#"><li>Opportunità di lavoro</li></a>
+        </ul>
+      </div>
+  
+      <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+        <ul>
+          <a href="#"><li><strong>OSPITA</strong></li></a>
+          <a href="#"><li>Diventa un host</li></a>
+          <a href="#"><li>Offri un'Esperienza online</li></a>
+          <a href="#"><li>Offri un'Esperienza</li></a>
+          <a href="#"><li>Ospitare responsabilmente</li></a>
+          <a href="#"><li>Open Homes</li></a>
+          <a href="#"><li>Centro Risorse</li></a>
+          <a href="#"><li>Community Center</li></a>
+        </ul>
+      </div>
+  
+      <div class="col-6 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+        <ul>
+          <a href="#"><li><strong>ASSISTENZA</strong></li></a>
+          <a href="#"><li>La nostra risposta all'emergenza COVID-19</li></a>
+          <a href="#"><li>Centro Assistenza</li></a>
+          <a href="#"><li>Opzioni di cancellazione</li></a>
+          <a href="#"><li>Servizio di supporto al vicinato</li></a>
+          <a href="#"><li>Affidabilità e sicurezza</li></a>
+        </ul>
+      </div>
+  
     </div>
-
-    <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3">
-      <ul>
-        <a href="#"><li><strong>COMMUNITY</strong></li></a>
-        <a href="#"><li>Diversità e appartenenza</li></a>
-        <a href="#"><li>Accessibilità</li></a>
-        <a href="#"><li>Airbnb Associates</li></a>
-        <a href="#"><li>Alloggi per l'emergenza</li></a>
-        <a href="#"><li>Invita degli amici</li></a>
-        <a href="#"><li>Olimpiadi</li></a>
-        <a href="#"><li>Opportunità di lavoro</li></a>
-      </ul>
-    </div>
-
-    <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3">
-      <ul>
-        <a href="#"><li><strong>OSPITA</strong></li></a>
-        <a href="#"><li>Diventa un host</li></a>
-        <a href="#"><li>Offri un'Esperienza online</li></a>
-        <a href="#"><li>Offri un'Esperienza</li></a>
-        <a href="#"><li>Ospitare responsabilmente</li></a>
-        <a href="#"><li>Open Homes</li></a>
-        <a href="#"><li>Centro Risorse</li></a>
-        <a href="#"><li>Community Center</li></a>
-      </ul>
-    </div>
-
-    <div class="col-6 col-sm-6 col-md-6 col-lg-3 col-xl-3">
-      <ul>
-        <a href="#"><li><strong>ASSISTENZA</strong></li></a>
-        <a href="#"><li>La nostra risposta all'emergenza COVID-19</li></a>
-        <a href="#"><li>Centro Assistenza</li></a>
-        <a href="#"><li>Opzioni di cancellazione</li></a>
-        <a href="#"><li>Servizio di supporto al vicinato</li></a>
-        <a href="#"><li>Affidabilità e sicurezza</li></a>
-      </ul>
-    </div>
-
   </div>
-
-  <div class="row footer-bottom">
-
-    <div class="col-lg-6 col-md-6 col-sm-12 footer-bottom-left">
-      <p>© 2020 Boolbnb, Inc. All rights reserved</p>
-      <p class="ft">Privacy</p>
-      <p class="ft">Termini</p>
-      <p class="ft">Dettagli dell'azienda</p>
+  <hr>
+  <div class="container footer-bottom">
+    <div class="row">
+  
+      <div class="col-lg-6 col-md-6 col-sm-12 footer-bottom-left">
+        <p>© 2020 Boolbnb, Inc. All rights reserved</p>
+        <p class="ft">Privacy</p>
+        <p class="ft">Termini</p>
+        <p class="ft">Dettagli dell'azienda</p>
+      </div>
+  
+      <div class="text-right col-lg-6 col-md-6 col-sm-12 footer-bottom-right">
+        <span><i class="fas fa-globe"></i><strong> Italiano (IT)</strong></span>
+        <span><i class="fas fa-euro-sign"></i><strong> EUR</strong></span>
+        <span><i class="fab fa-facebook-f" id="social"></i></span>
+        <span><i class="fab fa-twitter" id="social"></i></span>
+        <span><i class="fab fa-instagram" id="social"></i></span>
+      </div>
+  
     </div>
-
-    <div class="offset-lg-3 col-lg-3 col-md-6 col-sm-12 footer-bottom-right">
-      <span><i class="fas fa-globe"></i><strong> Italiano (IT)</strong></span>
-      <span><i class="fas fa-euro-sign"></i><strong> EUR</strong></span>
-      <span><i class="fab fa-facebook-f" id="social"></i></span>
-      <span><i class="fab fa-twitter" id="social"></i></span>
-      <span><i class="fab fa-instagram-square" id="social"></i></span>
-    </div>
-
   </div>
 </div>
