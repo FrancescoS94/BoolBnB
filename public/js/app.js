79c2b9f8ada53d913d5f67feacc0a63b79b74f1f
FrancescoS <francesco.salerno2294@gmail.com>
2020-11-30 12:36:19 +0100

    varie

--- public/js/app.js
+++ public/js/app.js
@@ -37376,8 +37376,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\mamp_public\BoolBnB\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\mamp_public\BoolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\mamp_public\Progetto-Finale\Boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\mamp_public\Progetto-Finale\Boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
